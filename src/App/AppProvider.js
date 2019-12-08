89971521f0c38020664ad7550239c1a13d116843
Ilya Petrov <dnamix1@gmail.com>
2019-12-08 12:26:32 +0300

    added generic Page component

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+const cc = require('cryptocompare');
 export const AppContext = React.createContext();
 
 export default class AppProvider extends React.Component {
@@ -12,6 +12,16 @@ export default class AppProvider extends React.Component {
             confirmFavorites: this.confirmFavorites
         }
     }
+
+    componentDidMount() {
+        this.fetchCoins();
+    }
+
+    fetchCoins = async () => {
+        let coinList = (await cc.coinList()).Data;
+        this.setState({coinList});
+        console.log(coinList)
+    };
 
     static savedSettings(){
         let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
