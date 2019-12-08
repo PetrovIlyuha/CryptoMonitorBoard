25389ca0d44c1734344da65078d1c147cff941ea
Ilya Petrov <dnamix1@gmail.com>
2019-12-08 12:38:20 +0300

    added coin grid in settings page

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from 'styled-components';
+import {AppContext} from "../App/AppProvider";
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  margin-left: 50px;
+`;
+
+export default function (){
+    return <AppContext.Consumer>
+        {({coinList}) => <CoinGridStyled>
+            {Object.keys(coinList).map(coinKey => <div>
+                {coinKey}
+            </div>)}
+        </CoinGridStyled>}
+    </AppContext.Consumer>
+}
