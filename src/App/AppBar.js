c68a09f6ebe26bff101592aa078c4d0d44971914
Ilya Petrov <dnamix1@gmail.com>
2019-12-08 11:09:15 +0300

    styled navbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,14 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`;
 const Bar = styled.div`
     display: grid;
-    grid-template-columns: 180px auto 100px 100px;
+    margin-bottom: 40px;
+    grid-template-columns: 180px auto 100px 120px;
+    column-gap: 20px;
 `;
-export default function(){
-    return  <Bar>
-        <div>CryptoDash</div>
+
+const ControlButtonElem = styled.div`
+   cursor: pointer;
+   //font-family: 'Puritan', sans-serif;
+   font-size: 1.2em;
+   ${props => props.active && css`
+    text-shadow: 0px 0px 20px rgba(26,237,51,1);;
+   `}
+`;
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({name, active}) {
+    return (
+        <ControlButtonElem active={active}>
+            {toProperCase(name)}
+        </ControlButtonElem>
+    )
+}
+
+export default function () {
+    return <Bar>
+        <Logo>CryptoDash</Logo>
         <div/>
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <ControlButton active name={"dashboard"}/>
+        <ControlButton name={"settings"}/>
     </Bar>
 }
