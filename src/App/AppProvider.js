a92a81b4281992025e8352ef9969b49b4ce49709
Ilya Petrov <dnamix1@gmail.com>
2019-12-08 12:05:54 +0300

    settings page and local storage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -7,14 +7,36 @@ export default class AppProvider extends React.Component {
         super(props);
         this.state = {
             page: 'dashboard',
-            setPage: this.setPage
+            ...AppProvider.savedSettings(),
+            setPage: this.setPage,
+            confirmFavorites: this.confirmFavorites
         }
     }
-    setPage = page => this.setState({page});
 
+    static savedSettings(){
+        let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+        if (!cryptoDashData){
+            return {page: "settings", firstVisit: true}
+        }
+        return {}
+    }
+    setPage = page => this.setState({page});
+    confirmFavorites = () => {
+        this.setState({
+            firstVisit: false,
+            page: 'dashboard'
+        });
+        localStorage.setItem("cryptoDash", JSON.stringify({
+            test: "BITCOIN IS EVERYTHING"
+        }))
+    }
     render(){
         return (
-            <AppContext.Provider value={this.state} setPage={this.setPage}>
+            <AppContext.Provider
+                value={this.state}
+                setPage={this.setPage}
+                confirmFavorites={this.confirmFavorites}
+            >
                 {this.props.children}
             </AppContext.Provider>
         )
